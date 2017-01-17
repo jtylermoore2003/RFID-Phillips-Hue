39a6f9d6a7342ee0362c587f5a616fbe5089cc87
jtylermoore2003 <jtylermoore2003@gmail.com>
2017-01-17 20:00:17 +0000

    Update phue-bridge-setup.py

--- phue-bridge-setup.py
+++ phue-bridge-setup.py
@@ -1,8 +1,10 @@
+#Run this program once to connect phue to your bridge
+
 from phue import Bridge
 import logging
 logging.basicConfig()
 
-b = Bridge('192.168.1.218')
+b = Bridge('ip of your bridge')
 
 b.connect()
 
